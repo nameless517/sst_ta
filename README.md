ed75a5dbefd4f3ec6ac236488a9d08f537f41a5a
dvlindsguize <60939929+nameless517@users.noreply.github.com>
2024-04-25 23:12:48 +0300

    Fixes according to feedback

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sst_ta
+Env vars must be set beforehand, in this implementation they are being exported with launch script.
 Run launch.sh
--- launch.sh
+++ launch.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+SCRIPT_DIR="$PWD"
+export DEPLOY_TARGET=some_vm_group
+export COMPOSE_FILE_PATH="$SCRIPT_DIR"
+export STACK_NAME=SST_TA
 ansible-playbook SST_TA.yml
